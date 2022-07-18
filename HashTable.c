c241480dad98732eec66b167e8a29050d943cff8
Mohd Shams Qureshi <mshamsq89@gmail.com>
2022-07-18 15:59:18 +0530

    [PC] HashTable implemtaion in C
    Implemented Insert
    implementes display

--- HashTable.c
+++ HashTable.c
@@ -1,6 +1,63 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 20
+
+typedef struct {
+    int key;
+    int data;
+}DataItem;
+
+DataItem* hashArray[MAX];
+DataItem* dummyItem;
+DataItem* item;
+
+int hashCode(int key)
+{
+    return (key % MAX);
+}
+
+void display()
+{
+    int i = 0;
+    for(i=0; i<MAX; i++)
+    {
+        if(hashArray[i] != NULL)
+            printf("(%d, %d))",hashArray[i]->key,hashArray[i]->data);
+        else
+            printf("(~,~)");
+    }
+    printf("\n");
+}
+void insert(int key, int data)
+{
+    item = (DataItem *)malloc(sizeof(DataItem));
+    item->data = data;
+    item->key = key;
+
+    int hashIndex = hashCode(key);
+
+    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
+    {
+        hashIndex++;
+        hashIndex % MAX;
+    }
+    hashArray[hashIndex] = item;
+}
 int main()
 {
-    printf("Hello World\n");
+    dummyItem = (DataItem *)malloc(sizeof(DataItem));
+    dummyItem->data = -1;
+    dummyItem->key = -1;
+
+    insert(5,55);
+    insert(1,11);
+    insert(4,44);
+    insert(3,33);
+    insert(6,66);
+    insert(7,77);
+    insert(2,22);
+    display();
+   
     return 0;
 }
